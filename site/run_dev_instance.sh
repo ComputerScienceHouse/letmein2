c1286aa1c82a07c50f418681ad6279bc15168d46
Will Nilges <wdn5796@rit.edu>
2022-06-14 22:48:02 -0400

    Fix issues with dev instance container

--- site/run_dev_instance.sh
+++ site/run_dev_instance.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-podman run --rm -it --name=letmein-site -v ./static:/static -v ./templates:/templates --env-file=.env.container -p 8080:8080 letmein-site:latest
+echo "Please view the README before trying to run this script"
+podman run --rm -it --name=letmein-site -v ./static:/static:Z -v ./templates:/templates:Z --env-file=.env.container -p 8080:8080 letmein-site:latest
