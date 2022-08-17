13e2839e7a1c8519d773d516ac1d90faf9172035
Will Nilges <wdn5796@rit.edu>
2022-08-17 00:51:09 -0400

    STFU kinda works again, but not in minutes

--- embedded/app.py
+++ embedded/app.py
@@ -7,6 +7,7 @@ class App:
     def __init__(self, mqtt_client, jingle):
         self.mqtt_client = mqtt_client
         self.jingle = jingle
+        self.stfu_counter = 0
 
         self.mqtt_client.on_message = self.message
 
@@ -16,11 +17,20 @@ class App:
     async def run(self):
         # Jingle + ASCII art to let the user know the board is ready to go
         await self.jingle.play("ready.jingle")
+        light_show()
         art_ready()
         check_ack_task = asyncio.create_task(self.check_ack())
         check_jingle_task = asyncio.create_task(self.check_jingle())
         check_mqtt_task = asyncio.create_task(self.check_mqtt())
-        await asyncio.gather(check_ack_task, check_jingle_task, check_mqtt_task)
+        check_stfu_task = asyncio.create_task(self.check_stfu())
+        stfu_decay_task = asyncio.create_task(self.stfu_decay())
+        await asyncio.gather(
+            check_ack_task,
+            check_jingle_task,
+            check_mqtt_task,
+            check_stfu_task,
+            stfu_decay_task
+        )
 
     # Run MQTT transactions
     async def check_mqtt(self):
@@ -38,6 +48,35 @@ class App:
                 all_off()
             await asyncio.sleep(0.5)
 
+    async def check_stfu(self):
+        while True:
+            if stfu.value:
+                if led_stfu.value:
+                    self.stfu_counter = 0
+                    self.mqtt_client.subscribe(mqtt_req_topic)
+                else:
+                    # Scale the counter to seconds (since the counter counts down once per second-ish)
+                    # FIXME (willnilges): I think each tick is a bit longer than a second so keep that
+                    # in mind when you're setting duration 
+                    print(stfu_duration_minutes * 60)
+                    self.stfu_counter = stfu_duration_minutes * 60
+                    self.mqtt_client.unsubscribe(mqtt_req_topic)
+                # We're gonna use the LED to keep track of the status b/c we're goblins.
+                led_stfu.value = not led_stfu.value
+            await asyncio.sleep(0.5)
+
+    async def stfu_decay(self):
+        while True:
+            if led_stfu.value:
+                if self.stfu_counter > 0:
+                    self.stfu_counter -= 1
+                    print(f"SHUT THE FUCK UP MODE = {led_stfu.value}")
+                    print(f"stfu_counter = {self.stfu_counter}")
+                else:
+                    led_stfu.value = 0
+                    self.mqtt_client.subscribe(mqtt_req_topic)
+            await asyncio.sleep(1)
+
     # Check if we should be playing music, and play music if so
     async def check_jingle(self):
         while True:
@@ -47,15 +86,14 @@ class App:
                 # to the new light. 
                 if s_stairs.value:
                     await self.jingle.play("s_stairs.jingle")
-                    continue
                 elif n_stairs.value:
                     await self.jingle.play("n_stairs.jingle")
                 elif level_a.value:
                     await self.jingle.play("level_a.jingle")
                 elif level_1.value:
-                    await self.jingle.play("song_of_storms.jingle")
+                    await self.jingle.play("song_of_storms.jingle") # level_1
                 elif l_well.value:
-                    await self.jingle.play("song_of_healing.jingle")
+                    await self.jingle.play("song_of_healing.jingle") # l_well
             await asyncio.sleep(1)
 
     # MQTT message handler
@@ -79,11 +117,7 @@ class App:
             # back off. Should fix this. IDEA: Sub/Unsub.
             # TODO (willnilges): Perhaps a timeout topic would be nice.
             self.jingle.buzzer.off()
-            level_a.value = 0
-            level_1.value = 0
-            s_stairs.value = 0
-            n_stairs.value = 0
-            l_well.value = 0
+            all_off()
         elif topic == mqtt_timeout_topic:
             self.jingle.buzzer.off()
             if "level_a" in message:
